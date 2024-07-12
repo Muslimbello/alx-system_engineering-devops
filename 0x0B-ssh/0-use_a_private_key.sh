39da780204db3d6cb075b2f9cd1be72a090186e0
Your Name <you@example.com>
2024-07-12 10:59:39 +0100

    task 0 attepmpted

--- 0x0B-ssh/0-use_a_private_key.sh
+++ 0x0B-ssh/0-use_a_private_key.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
-
 #Define the private key path
 PRIVATE_KEY_PATH="~/.ssh/school"
-
 #Define the username
 USERNAME="ubuntu"
-
 #Server Address
 SERVER_ADDRESS="54.208.117.65"
-
 #Connect to the server via ssh
 ssh -i $PRIVATE_KEY_PATH $USERNAME@SERVER_ADDRESS
 
