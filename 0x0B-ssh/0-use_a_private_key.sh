e04af1116df6c3e99dae7047298505535cfd9e47
Your Name <you@example.com>
2024-07-13 14:56:53 +0100

    Updated IP on address

--- 0x0B-ssh/0-use_a_private_key.sh
+++ 0x0B-ssh/0-use_a_private_key.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
-#Define the private key path
-PRIVATE_KEY_PATH="~/.ssh/school"
-#Define the username
-USERNAME="ubuntu"
-#Server Address
-SERVER_ADDRESS="54.208.117.65"
-#Connect to the server via ssh
-ssh -i $PRIVATE_KEY_PATH $USERNAME@SERVER_ADDRESS
+#Connect to the server via ssh using private key
 
+ssh -i ~/.ssh/school ubuntu@100.25.148.117
