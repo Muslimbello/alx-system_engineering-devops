6054b41971d2cb824b364b042b8a20abcf48fcec
Your Name <you@example.com>
2024-07-13 16:13:25 +0100

    removed sh extension from file name

--- ssh file gen.sh
+++
@@ -1,8 +0,0 @@
-# Create the .ssh directory if it doesn't exist
-mkdir -p ~/.ssh
-
-# Move the private key to the .ssh directory and rename it to 'school'
-mv ~/alx_projects/alx-system_engineering-devops/0x0B-ssh/id_rsa  ~/.ssh/school
-
-# Set the correct permissions for the private key
-chmod 600 ~/.ssh/school
